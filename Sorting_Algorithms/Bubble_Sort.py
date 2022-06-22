b4685e853d50494b0e67bda5dfa5868adabcc0ff
28Thiyagarajan-M <thiyagarajanmohan2001@gmail.com>
2022-06-22 23:20:53 +0530

    enter

---
+++ Sorting_Algorithms/Bubble_Sort.py
@@ -0,0 +1,15 @@
+# Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. T
+# This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n- i-1):
+            if(arr[j]>arr[j+1]):
+                arr[j],arr[j+1]= arr[j+1],arr[j]
+    
+    print(arr)
+
+arr  = [7,3,1,2,6]
+bubbleSort(arr)
